bbda99ba6ecb2c58455b08b05525a7fbcea92cd6
Zeljko Bekcic <zeljko.bekcic@iits-consulting.de>
2025-03-24 15:19:35 +0100

    fix versions

--- versions.tf
+++ versions.tf
@@ -1,13 +1,19 @@
 terraform {
+
+  required_version = ">= 1.5.7"
+
   required_providers {
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
     }
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
+      version = "~> 2.17"
     }
     opentelekomcloud = {
-      source = "opentelekomcloud/opentelekomcloud"
+      source  = "opentelekomcloud/opentelekomcloud"
+      version = "~> 1.32"
     }
   }
 }
